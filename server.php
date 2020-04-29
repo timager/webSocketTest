24de51e0ffd42e68f91b7e811afb7e18b65909c4
timager <timger98@gmail.com>
2020-04-29 18:23:38 +0300

    worked

--- server.php
+++ server.php
@@ -1,29 +1,21 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+
+use Workerman\Connection\ConnectionInterface;
 use Workerman\Worker;
 
 $connections = [];
 
 $ws_worker = new Worker("websocket://0.0.0.0:8000");
-$ws_worker->onWorkerStart = function() use (&$connections)
-{
-//    // создаём локальный tcp-сервер, чтобы отправлять на него сообщения из кода нашего сайта
-//    $inner_tcp_worker = new Worker("tcp://127.0.0.1:1234");
-//    // создаём обработчик сообщений, который будет срабатывать,
-//    // когда на локальный tcp-сокет приходит сообщение
-//    $inner_tcp_worker->onMessage = function($connection, $data) use (&$users) {
-//        $data = json_decode($data);
-//        // отправляем сообщение пользователю по userId
-//        if (isset($users[$data->user])) {
-//            $webconnection = $users[$data->user];
-//            $webconnection->send($data->message);
-//        }
-//    };
-//    $inner_tcp_worker->listen();
-};
 
-$ws_worker->onMessage = function($connection, $data){
-    echo $data;
+$ws_worker->onMessage = function($connection, $data) use (&$connections) {
+    foreach($connections as $user => $connection) {
+        /**
+         * @var ConnectionInterface $connection
+         */
+        $connection->send($data);
+    }
+    echo $data . PHP_EOL;
 };
 
 $ws_worker->onConnect = function($connection) use (&$connections)
@@ -33,13 +25,6 @@ $ws_worker->onConnect = function($connection) use (&$connections)
         $connections[$_GET['user']] = $connection;
     };
 };
-//
-//$ws_worker->onClose = function($connection) use(&$connections)
-//{
-//    // удаляем параметр при отключении пользователя
-//    $user = array_search($connection, $connections);
-//    unset($connections[$user]);
-//};
 
-// Run worker
+
 Worker::runAll();
